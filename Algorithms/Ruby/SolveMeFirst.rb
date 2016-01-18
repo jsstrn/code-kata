361264cfd367d6b2f320988ec1e8da6844564980
jsstrn <jessternrays@gmail.com>
2016-01-18 22:49:20 +0800

    Add SolveMeFirst.rb

---
+++ Algorithms/Ruby/SolveMeFirst.rb
@@ -0,0 +1,8 @@
+def addTwoNumbers (firstNumber, secondNumber)
+  return firstNumber + secondNumber
+end
+
+firstNumber = gets.to_i
+secondNumber = gets.to_i
+sumOfTwoNumbers = addTwoNumbers(firstNumber, secondNumber)
+puts sumOfTwoNumbers
