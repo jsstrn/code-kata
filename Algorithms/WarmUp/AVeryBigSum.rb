f904c5396841883a7f90c95c812bc901763a95ab
jsstrn <jessternrays@gmail.com>
2016-01-19 00:27:13 +0800

    Add AVeryBigSum.rb

---
+++ Algorithms/WarmUp/AVeryBigSum.rb
@@ -0,0 +1,10 @@
+number_of_integers = gets.strip.to_i
+
+array_of_integers = gets.strip # strip trailing whitespace
+array_of_integers = array_of_integers.split(' ') #=> ['1', '2', '3']
+array_of_integers = array_of_integers.map {|element| element.to_i} #=> [1, 2, 3]
+
+sum_of_array = 0
+array_of_integers.each {|element| sum_of_array += element}
+
+puts sum_of_array
