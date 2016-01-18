82cb40b03385061ad4733acfa72fd0a2f503a36b
jsstrn <jessternrays@gmail.com>
2016-01-19 00:04:28 +0800

    Move file to WarmUp directory

--- Algorithms/Ruby/SolveMeFirst.rb
+++
@@ -1,8 +0,0 @@
-def addTwoNumbers (firstNumber, secondNumber)
-  return firstNumber + secondNumber
-end
-
-firstNumber = gets.to_i
-secondNumber = gets.to_i
-sumOfTwoNumbers = addTwoNumbers(firstNumber, secondNumber)
-puts sumOfTwoNumbers
