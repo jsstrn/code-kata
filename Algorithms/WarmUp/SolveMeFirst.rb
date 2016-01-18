aa0d69deb117ecb234ea97e14e4fd7167b66efe6
jsstrn <jessternrays@gmail.com>
2016-01-19 00:05:01 +0800

    Use snake_case to name variables

---
+++ Algorithms/WarmUp/SolveMeFirst.rb
@@ -0,0 +1,9 @@
+def sum_two_numbers (first_number, second_number)
+  return first_number + second_number
+end
+
+# get input and convert to integer
+first_number = gets.to_i
+second_number = gets.to_i
+# prints statement with a newline
+puts sum_two_numbers(first_number, second_number)
