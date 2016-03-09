d176a3522d1e47bd83f1e40a99c14599c901caea
Jesstern Rays <jessternrays@gmail.com>
2016-03-09 10:53:30 +0800

    Solve bottles of beer problem in Ruby

---
+++ General/bottles-of-beer.rb
@@ -0,0 +1,10 @@
+def beer (bottles)
+  (bottles).downto(0) do |bottle|
+    word = bottle == 1 ? 'bottle' : 'bottles'
+    bottle = bottle == 0 ? 'no more' : bottle
+    puts "#{(bottle.is_a? String) ? bottle.capitalize : bottle} #{word} of beer on the wall, #{bottle} #{word} of beer."
+    puts "Take one down, pass it around, #{bottle - 1} #{word} of beer on the wall."
+  end
+end
+
+beer(99)
