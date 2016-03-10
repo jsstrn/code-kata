efcf905edf118511148b6f3414297a32731da862
Jesstern Rays <jessternrays@gmail.com>
2016-03-10 09:10:07 +0800

    Update solution

--- General/bottles-of-beer.js
+++ General/bottles-of-beer.js
@@ -1,17 +1,13 @@
 (function () {
   'use strict'
+  function capitalize (word) {
+    return word.charAt(0).toUpperCase() + word.slice(1)
+  }
   function beer (bottles) {
-    for (var bottle = bottles; -1 < bottle; bottle--) {
-      if (bottle === 1) {
-        console.log(bottle + ' bottle of beer on the wall, ' + bottle + ' bottle of beer.')
-        console.log('Take it down, pass it around, ' + 'no more' + ' bottles of beer on the wall.')
-      } else if (bottle === 0) {
-        console.log('No more' + ' bottles of beer on the wall, ' + 'no more' + ' bottles of beer.')
-        console.log('Go to the store and buy some more ' + bottles + ' bottles of beer on the wall.')
-      } else {
-        console.log(bottle + ' bottles of beer on the wall, ' + bottle + ' bottles of beer.')
-        console.log('Take one down, pass it around, ' + (bottle - 1) + ' bottles of beer on the wall.')
-      }
+    for (var bottle = bottles; bottle > -1; bottle--) {
+      var word = (bottle === 1) ? 'bottle' : 'bottles'
+      console.log(`${bottle} ${word} of beer on the wall, ${bottle} ${word} of beer on the wall.`)
+      console.log(`Take one down, pass it around, ${bottle} ${word} of beer on the wall.`)
     }
   }
   beer(99)
