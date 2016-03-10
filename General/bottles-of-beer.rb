bb5eae7948576ea2596660f4650f61e1aeb5a235
Jesstern Rays <jessternrays@gmail.com>
2016-03-10 09:10:13 +0800

    Update solution

--- General/bottles-of-beer.rb
+++ General/bottles-of-beer.rb
@@ -2,8 +2,10 @@ def beer (bottles)
   (bottles).downto(0) do |bottle|
     word = bottle == 1 ? 'bottle' : 'bottles'
     bottle = bottle == 0 ? 'no more' : bottle
-    puts "#{(bottle.is_a? String) ? bottle.capitalize : bottle} #{word} of beer on the wall, #{bottle} #{word} of beer."
-    puts "Take one down, pass it around, #{(bottle.is_a? String) ? bottle : (bottle - 1)} #{word} of beer on the wall."
+
+		puts "#{(bottle.is_a? String) ? bottle.capitalize : bottle} #{word} of beer on the wall, #{bottle} #{word} of beer."
+
+		puts "Take one down, pass it around, #{(bottle.is_a? String) ? bottle : (bottle - 1)} #{word} of beer on the wall."
   end
 end
 
