721b8290dec1c57c64eed67a9bc7b7a59121fbd3
Jesstern Rays <jsstrn@Jessterns-MacBook-Air.local>
2016-04-05 10:36:06 +0800

    Add object literals in JavaScript

--- JavaScript/object-literals.js
+++ JavaScript/object-literals.js
@@ -3,18 +3,20 @@ var Pet = {
     this.name = options.name
     this.born = options.born
     this.died = options.died
-  }
-}
-
-var Dog = {
-  init: function (breed, options) {
-    Pet.init.call(this, options)
-    this.breed = breed
   },
   age: function () {
     return (this.died || new Date().getFullYear()) - this.born
   }
 }
+
+var Dog = Object.create(Pet, {
+  init: {
+    value: function (breed, options) {
+      Pet.init.call(this, options)
+      this.breed = breed
+    }
+  }
+})
 
 var dog = Object.create(Dog)
 dog.init('Labrador', {
