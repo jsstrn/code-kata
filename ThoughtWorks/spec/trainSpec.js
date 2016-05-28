2861c378294d33f13f6ffa9a7ebd05a49a2552af
Jesstern Rays <jsstrn@Jessterns-MacBook-Air.local>
2016-05-28 21:13:18 +0800

    Update problem sets

--- ThoughtWorks/spec/trainSpec.js
+++ ThoughtWorks/spec/trainSpec.js
@@ -1,37 +1,42 @@
 /*globals describe, it, expect*/
 
-const solution = require('../solution/train.js')
+const solution = require('../trains/solution.js')
 
 describe('Problem Set 1 - Trains', () => {
   describe('Compute the distance along a certain route', () => {
-    it('The distance of the route A-B-C.', () => {
+    it('1. distance of the route A-B-C.', () => {
       expect(
         solution.getDistance('A', 'B', 'C')
       ).toBe(9)
     })
-    it('The distance of the route A-D.', () => {
+    it('2. distance of the route A-D.', () => {
       expect(
         solution.getDistance('A', 'D')
       ).toBe(5)
     })
-    it('The distance of the route A-D-C.', () => {
+    it('3. distance of the route A-D-C.', () => {
       expect(
         solution.getDistance('A', 'D', 'C')
       ).toBe(13)
     })
-    it('The distance of the route A-E-B-C-D.', () => {
+    it('4. distance of the route A-E-B-C-D.', () => {
       expect(
         solution.getDistance('A', 'E', 'B', 'C', 'D')
       ).toBe(22)
     })
-    it('The distance of the route A-E-D.', () => {
+    it('5. distance of the route A-E-D.', () => {
       expect(
         solution.getDistance('A', 'E', 'D')
       ).toBe('NO SUCH ROUTE')
     })
   })
   describe('Get the number of different routes between two towns.', () => {
+    it('6. trips starting at C and ending at C with a maximum of 3 stops.', () => {
 
+    })
+    it('7. trips starting at A and ending at C with exactly 4 stops.', () => {
+
+    })
   })
   describe('Get the shortest route between two towns', () => {
 
