f7de0b59dcfb6a4f9d285501cfb3ad9a5bcbd4b7
Jesstern Rays <jsstrn@Jessterns-MacBook-Air.local>
2016-06-01 02:03:06 +0800

    Update solution

--- ThoughtWorks/hotels/solution.js
+++ ThoughtWorks/hotels/solution.js
@@ -1,8 +1,9 @@
 'use strict'
 
 class Hotel {
-  constructor (name, regularRates, rewardsRates) {
+  constructor (name, rating, regularRates, rewardsRates) {
     this.name = name
+    this.rating = rating
     this.rates = {
       regular: {
         weekday: regularRates[0],
@@ -17,28 +18,29 @@ class Hotel {
 }
 
 class Hotels {
-  constructor () {
-    this.hotels = [
-      new Hotel('Lakewood', [110, 80], [90, 80]),
-      new Hotel('Bridgewood', [160, 110], [60, 50]),
-      new Hotel('Ridgewood', [220, 150], [150, 40])
-    ]
+  constructor (hotels) {
+    this.hotels = hotels
   }
   isWeekend (date) {
     return (date.getDay() === 0 || date.getDay() === 6)
   }
-  getRateFor (hotelName, customerType, date) {
-    const hotel = this.hotels.find((hotel) => {
+  getTypeOfDay (date) {
+    return this.isWeekend(date) ? 'weekend' : 'weekday'
+  }
+  getHotelByName (hotelName) {
+    return this.hotels.find((hotel) => {
       return hotel.name === hotelName
     })
-    const typeOfDay = this.isWeekend(date) ? 'weekend' : 'weekday'
-    return hotel.rates[customerType][typeOfDay]
+  }
+  getRateForHotel (hotelName, customerType, date) {
+    const hotel = this.getHotelByName(hotelName)
+    return hotel.rates[customerType][this.getTypeOfDay(date)]
   }
   getCheapestRates (customerType) {
     const dates = [arguments[1], arguments[2], arguments[3]]
     const rates = this.hotels.map(hotel => {
       return dates.map(date => {
-        return this.getRateFor(hotel.name, customerType, date)
+        return this.getRateForHotel(hotel.name, customerType, date)
       }).reduce((prev, curr) => prev + curr)
     })
     const cheapestRate = rates.reduce((prev, curr) => Math.min(prev, curr))
@@ -46,4 +48,8 @@ class Hotels {
   }
 }
 
-module.exports = new Hotels()
+module.exports = new Hotels([
+  new Hotel('Lakewood', 3, [110, 80], [90, 80]),
+  new Hotel('Bridgewood', 4, [160, 110], [60, 50]),
+  new Hotel('Ridgewood', 5, [220, 150], [150, 40])
+])
