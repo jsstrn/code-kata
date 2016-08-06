44f150d7b42dc40f723d7d4a6c197088be2e9e4b
Jesstern Rays <jrays@thoughtworks.com>
2016-08-06 10:18:33 +0800

    Refactor validator.js to use ES6 classes

--- JavaScript/validator/validator.js
+++ JavaScript/validator/validator.js
@@ -1,5 +1,6 @@
-function Validator () {
-  this.validate = (input) => {
+class Validator {
+  constructor () {}
+  validate (input) {
     const validations = {
       'number': (list) => list.some(this.isNumber),
       'uppercase': (list) => list.some(this.isUpperCase),
@@ -8,10 +9,10 @@ function Validator () {
     }
     return Object.keys(validations).filter((key) => !validations[key](input.split('')))
   }
-  this.isNumber = (char) => /[0-9]/.test(char)
-  this.isUpperCase = (char) => /[A-Z]/.test(char)
-  this.isLowerCase = (char) => /[a-z]/.test(char)
-  this.isSymbol = (char) => /[^a-zA-Z0-9'\n']/.test(char)
+  isNumber (char) { return /[0-9]/.test(char) }
+  isUpperCase (char) { return /[A-Z]/.test(char) }
+  isLowerCase (char) { return /[a-z]/.test(char) }
+  isSymbol (char) { return /[^a-zA-Z0-9'\n']/.test(char) }
 }
 
 module.exports = new Validator()
